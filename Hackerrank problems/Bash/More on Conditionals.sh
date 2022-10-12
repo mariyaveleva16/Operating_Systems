edc94482c78ad01219fca0fd3a0d863be0b9e266
Maria Veleva <56577507+mariyaveleva16@users.noreply.github.com>
2022-10-12 14:55:18 +0300

    Create More on Conditionals.sh

---
+++ Hackerrank problems/Bash/More on Conditionals.sh
@@ -0,0 +1,12 @@
+read x
+read y
+read z
+if [ $x == $y ] && [ $x == $z ]
+then   
+    echo "EQUILATERAL"
+elif [ $x != $y ] && [ $x != $z ] && [ $y != $z ]
+then 
+    echo "SCALENE"
+else
+    echo "ISOSCELES"
+fi
